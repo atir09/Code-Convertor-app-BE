c498b6203c8de5ce7f55a2a013570e74b8696277
atir_09 <atirkhan0906@gmail.com>
2023-08-06 15:05:39 +0530

    Added BE code for Code Convertor with OpenAI API

--- index.js
+++ index.js
@@ -39,33 +39,6 @@ app.post('/convert-code', async (req, res) => {
     }
 });
 
-app.post('/debug-code', async (req, res) => {
-    try {
-        const { code } = req.body;
-
-        const response = await axios.post(
-            'https://api.openai.com/v1/completions',
-            {
-                model: "text-davinci-003",
-                prompt: `Debug the following code:\n${code}\nAlso Provide short explanation if any issues or just reply with Code is Perfect. `,
-                max_tokens: 150,
-                temperature: 0.7,
-            },
-            {
-                headers: {
-                    'Content-Type': 'application/json',
-                    Authorization: `Bearer ${process.env.OPENAI_API_KEY}`,
-                },
-            }
-        );
-
-        const convertedCode = response.data.choices[0].text.trim();
-        res.json({ convertedCode });
-    } catch (error) {
-        console.error('Error converting code:', error.message);
-        res.status(500).json({ error: 'Error converting code' });
-    }
-});
 
 app.post('/debug-code', async (req, res) => {
     try {
