e21df2a24445e87ee51bf2c4b3e88c1e4f780df3
atir_09 <atirkhan0906@gmail.com>
2023-08-06 15:09:41 +0530

    Added BE code for Code Convertor with OpenAI API

--- index.js
+++ index.js
@@ -2,9 +2,11 @@ const express = require("express")
 require("dotenv").config()
 const axios = require('axios');
 const bodyParser = require('body-parser');
+const cors=require("cors")
 
 const app = express()
 app.use(bodyParser.json());
+app.use(cors())
 
 app.get("/", (req, res) => {
     res.send("Home Page")
